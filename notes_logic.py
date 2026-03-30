fd8d6854505363b5afea135ab628ec0437df6ffb
Lyutskan Georgiev <9nexusa9@gmail.com>
2026-03-30 19:17:49 +0300

    feat: implement delete note logic

--- notes_logic.py
+++ notes_logic.py
@@ -6,17 +6,15 @@ NOTES_FILE = "notes.json"
 
 def load_notes():
     """Reads notes from the JSON file and returns them as a list of dictionaries."""
-    #check if the file exist
+    #if the file doesnt exist return empty list
     if not os.path.exists(NOTES_FILE):
         return []
-    #load json in the list of dicts
     with open(NOTES_FILE, 'r') as f:
         notes = json.load(f)
         return notes
 
 def save_notes(notes):
     """Saves the list of notes to the JSON file."""
-    #dump the list of dicts (notes) in the json with format
     with open(NOTES_FILE, 'w') as f:
         json.dump(notes, f, indent=4)
     return
@@ -24,8 +22,10 @@ def save_notes(notes):
 def add_notes(title, content):
     """Adds notes to the JSON file."""
     notes = load_notes()
+    #if first note id = 1
     if not notes:
         new_id = 1
+    #if not empty new_id = id of last element + 1
     else:
         new_id = notes[-1]['id'] + 1
     notes.append({'id': new_id, 'title': title, 'content': content})
@@ -43,6 +43,18 @@ def get_note(note_id):
         if item['id'] == note_id:
             note = item
             break
+    #if note is unchanged its None => raise Error
     if not note:
         raise ValueError(f"Note with ID {note_id} not found.")
     return note
+
+def delete_note(note_id):
+    """Deletes a note from the JSON file."""
+    notes = load_notes()
+    for i, note in enumerate(notes):
+        if note['id'] == note_id:
+            del notes[i]
+            save_notes(notes)
+            return
+    #if the loops finishes without returning => id not found raise Error
+    raise ValueError(f"Note with ID {note_id} not found.")
